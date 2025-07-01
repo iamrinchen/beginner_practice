106ae7272f03efcc37d178626e9fad770fdaccb9
Ngawang <iamrinchen@gmail.com>
2025-07-01 18:04:01 +0530

    Add files via upload

---
+++ MonteCarlo.py
@@ -0,0 +1,88 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Nov 25 23:19:13 2023
+
+@author: nthakur
+"""
+
+
+import pandas as pd
+import numpy as np
+import scipy.stats
+from scipy.stats import norm
+from scipy.optimize import minimize
+import math
+
+
+
+def gbm(n_years = 10, n_scenarios=1000, mu=0.07, sigma=0.15, steps_per_year=12, s_0=100.0):
+    """
+    Evolution of Geometric Brownian Motion trajectories, such as for Stock Prices through Monte Carlo
+    :param n_years:  The number of years to generate data for
+    :param n_paths: The number of scenarios/trajectories
+    :param mu: Annualized Drift, e.g. Market Return
+    :param sigma: Annualized Volatility
+    :param steps_per_year: granularity of the simulation
+    :param s_0: initial value
+    :return: a numpy array of n_paths columns and n_years*steps_per_year rows
+    """
+    # Derive per-step Model Parameters from User Specifications
+    dt = 1/steps_per_year
+    n_steps = int(n_years*steps_per_year) + 1
+    rets_plus_1 = np.random.normal(loc=mu*dt+1, scale=sigma*np.sqrt(dt), size=(n_steps, n_scenarios))
+    # or better ...
+    # rets_plus_1 = np.random.normal(loc=(1+mu)**dt, scale=(sigma*np.sqrt(dt)), size=(n_steps, n_scenarios))
+    rets_plus_1[0] = 1
+    prices = s_0*pd.DataFrame(rets_plus_1).cumprod()
+    return prices
+
+def gbm2(n_years = 10, n_scenarios=1000, mu=0.07, sigma=0.15, steps_per_year=12, s_0=100.0, prices=True):
+    """
+    Evolution of Geometric Brownian Motion trajectories, such as for Stock Prices through Monte Carlo
+    :param n_years:  The number of years to generate data for
+    :param n_paths: The number of scenarios/trajectories
+    :param mu: Annualized Drift, e.g. Market Return
+    :param sigma: Annualized Volatility
+    :param steps_per_year: granularity of the simulation
+    :param s_0: initial value
+    :return: a numpy array of n_paths columns and n_years*steps_per_year rows
+    """
+    # Derive per-step Model Parameters from User Specifications
+    dt = 1/steps_per_year
+    n_steps = int(n_years*steps_per_year) + 1
+    # the standard way ...
+    # rets_plus_1 = np.random.normal(loc=mu*dt+1, scale=sigma*np.sqrt(dt), size=(n_steps, n_scenarios))
+    # without discretization error ...
+    rets_plus_1 = np.random.normal(loc=(1+mu)**dt, scale=(sigma*np.sqrt(dt)), size=(n_steps, n_scenarios))
+    rets_plus_1[0] = 1
+    ret_val = s_0*pd.DataFrame(rets_plus_1).cumprod() if prices else rets_plus_1-1
+    return ret_val
+
+
+p=gbm(n_years = 10, n_scenarios=3, mu=0.05, sigma=0.15, steps_per_year=12, s_0=100.0)
+
+p=gbm2(n_years = 10, n_scenarios=1000, mu=0.07, sigma=0.15, steps_per_year=12, s_0=100.0, prices=True)
+
+p.plot(figsize=(12,6),legend = False)
+
+###############
+
+import random
+
+def simulate_dice_rolls(num_simulations):
+    outcomes = [0, 0, 0, 0, 0, 0]  # Initialize counts for each outcome
+    
+    for _ in range(num_simulations):
+        roll_result = random.randint(1, 6)  # Simulate rolling a six-sided die
+        outcomes[roll_result - 1] += 1  # Increment the count for the corresponding outcome
+    
+    probabilities = [count / num_simulations for count in outcomes]
+    
+    return outcomes, probabilities
+
+num_simulations = 10000
+outcomes, probabilities = simulate_dice_rolls(num_simulations)
+
+for outcome, probability in enumerate(probabilities, start=1):
+    print(f"Outcome {outcome}: Probability {probability:.4f}")
+
